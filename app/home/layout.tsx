211b42296024c7212299a26e770433c4ef471d20
Pariskrit19 <moktan.pariskrit@webexpertsnepal.com>
2023-04-24 13:16:59 +0545

    home layout text changed

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({
 
 }) {
     const isUser = true;
-    return <section><h1>This Is Home</h1>{children}
+    return <section><h1>Welcome To Home</h1>{children}
 
     </section>;
 }
